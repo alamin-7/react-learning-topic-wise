c431530a9149c0d41c210c8709494583f199af50
alamin-7 <alamin188914@gmail.com>
2024-05-20 00:16:34 +0600

    event-handeler and useState hook

--- React-Starting-topics/functional_components/Users.js
+++ React-Starting-topics/functional_components/Users.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Users() {
+
+ // let data = "First!!!";
+  let [data, New] = useState("old");
+
+ const demo = () => {
+  alert("Hello !!");
+ };
+
+ const updateData = () => {
+   New("Updated data using useState");
+ }
+
   return (
-    <h1>Hello world</h1>
+    <div>
+    <h1>{data}</h1>
+
+    <button onClick={demo}>Click!</button>
+    <button onClick={updateData}>Update</button>
+    </div>
   )
 }
 
